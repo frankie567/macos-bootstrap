d789ade9fefffcba524c05d7411b6687168dfd60
François Voron <fvoron@gmail.com>
2019-11-08 18:47:56 +0100

    Update for Catalina and install Starship

---
+++ .zshrc
@@ -0,0 +1,4 @@
+eval "$(starship init zsh)"
+
+bindkey "^[[A" history-search-backward
+bindkey "^[[B" history-search-forward
--- macos-bootstrap.sh
+++ macos-bootstrap.sh
@@ -5,37 +5,31 @@ set -e
 # Install XCode command line  tools
 xcode-select --install
 
-# macOS Mojave workaround to have headers in /include
-# See https://stackoverflow.com/a/52900711/3864300
-sudo installer -pkg /Library/Developer/CommandLineTools/Packages/macOS_SDK_headers_for_macOS_10.14.pkg -target /
-
 # Install Brew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-# Install Cask
-brew tap caskroom/cask
-
 # Install tools through Brew
-brew install bash bash-completion git pyenv nvm httpie jq
+brew install git pyenv nvm httpie jq starship
 
-# Download and load .bash_profile
-curl -o ~/.bash_profile https://raw.githubusercontent.com/frankie567/macos-bootstrap/master/.bash_profile
-source ~/.bash_profile
+# Install apps through Cask
+brew tap homebrew/cask-fonts
+brew cask install font-fira-code google-chrome visual-studio-code
+
+# Download and load .zprofile
+curl -o ~/.zprofile https://raw.githubusercontent.com/frankie567/macos-bootstrap/master/.zprofile
+source ~/.zprofile
 
 # Download .inputrc
-curl -o ~/.inputrc https://raw.githubusercontent.com/frankie567/macos-bootstrap/master/.inputrc
+curl -o ~/.zshrc https://raw.githubusercontent.com/frankie567/macos-bootstrap/master/.zshrc
 
-# Install and set default Python to 3.7.1
-pyenv install 3.7.1
-pyenv global 3.7.1
+# Install and set default Python to 3.7.5
+pyenv install 3.7.5
+pyenv global 3.7.5
 
 # Install and set default to latest Node
 mkdir ~/.nvm
 nvm install node
 nvm alias default node
-
-# Install apps through Cask
-brew cask install google-chrome visual-studio-code
 
 # Generate SSH key
 ssh-keygen -t rsa -b 4096
